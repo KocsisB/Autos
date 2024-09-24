a13c066d1f6134a80d2344a6e46c928240b2a586
Btnd <kocsisb@kkszki.hu>
2024-09-24 12:47:50 +0200

    connect class created

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,32 @@ using System.Threading.Tasks;
 
 namespace auto
 {
+    public class Connect
+    {
+        public static MySqlConnection connection;
+        private string Host;
+        private string Database;
+        private string Username;
+        private string Password;
+        private string ConnectionString;
+
+        public Connect()
+        {
+            Host = "localhost";
+            Database = "auto";
+            Username = "root";
+            Password = "";
+
+            ConnectionString = "SERVER=" + Host+";DATABASE="+Database+";UID="+Username+";PASSWORD="+Password+";SslMode=None" ;
+
+            connection = new MySqlConnection(ConnectionString);
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            
         }
     }
 }
